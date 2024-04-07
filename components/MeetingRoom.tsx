d9979be53d7435ce3d03e977cc58f8ea9f42dd34
PhamCuongg <bomwithme@gmail.com>
2024-04-07 16:13:04 +0700

    fix mobile meeting room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+'use client';
+import { useState } from 'react';
 import {
   CallControls,
   CallParticipantsList,
@@ -8,6 +9,8 @@ import {
   SpeakerLayout,
   useCallStateHooks,
 } from '@stream-io/video-react-sdk';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Users, LayoutList } from 'lucide-react';
 
 import {
   DropdownMenu,
@@ -16,29 +19,29 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
-
 import Loader from './Loader';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { Users, LayoutList } from 'lucide-react';
+import EndCallButton from './EndCallButton';
 import { cn } from '@/lib/utils';
-import EndCallButton from "./EndCallButton";
-type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
+
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
   const searchParams = useSearchParams();
   const isPersonalRoom = !!searchParams.get('personal');
   const router = useRouter();
-  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
   const [showParticipants, setShowParticipants] = useState(false);
   const { useCallCallingState } = useCallStateHooks();
+
   const callingState = useCallCallingState();
+
   if (callingState !== CallingState.JOINED) return <Loader />;
 
   const CallLayout = () => {
     switch (layout) {
-      case "grid":
+      case 'grid':
         return <PaginatedGridLayout />;
-      case "speaker-right":
+      case 'speaker-right':
         return <SpeakerLayout participantsBarPosition="left" />;
       default:
         return <SpeakerLayout participantsBarPosition="right" />;
@@ -48,8 +51,8 @@ const MeetingRoom = () => {
   return (
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full items-center justify-center">
-        <div className=" flex size-full max-w-[1000px]">
-        <CallLayout/>
+        <div className=" flex size-full max-w-[1000px] items-center">
+          <CallLayout />
         </div>
         <div
           className={cn('h-[calc(100vh-86px)] hidden ml-2', {
@@ -59,9 +62,11 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-      <CallControls onLeave={() => router.push(`/`)} />
-      <DropdownMenu>
+      {/* video layout and call controls */}
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls onLeave={() => router.push(`/`)} />
+
+        <DropdownMenu>
           <div className="flex items-center">
             <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
               <LayoutList size={20} className="text-white" />
@@ -90,7 +95,6 @@ const MeetingRoom = () => {
         </button>
         {!isPersonalRoom && <EndCallButton />}
       </div>
-
     </section>
   );
 };
